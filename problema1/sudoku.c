9d7640dfafde43d7c37af0e226e38a2aa51234d6
marcelo046 <marc3lo046@gmail.com>
2018-11-05 15:13:28 -0200

    arquivo inicial do sudoku adicionado

---
+++ problema1/sudoku.c
@@ -0,0 +1,19 @@
+//  compilar e rodar:
+//    gcc qweuipvhowiruvhwaiuh
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+//#include <stdbool.h>
+
+int grid[9][9];
+
+// escopos das funções / threads
+int checaLinha();
+int checaColuna();
+int checaSubgrid();
+
+int main(int argc, char *argv[]) {
+  
+  return 0;
+}
